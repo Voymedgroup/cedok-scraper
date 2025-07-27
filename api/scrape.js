7d19b8d4e61d2d8ece63a4f347ab29fa48abb91a
Voymedgroup <voymedgroup@gmail.com>
2025-07-27 15:50:39 +0200

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 module.exports = async function (req, res) {
   try {
@@ -8,15 +9,12 @@ module.exports = async function (req, res) {
       return res.status(400).json({ error: 'Chybí parametr "url".' });
     }
 
-    // Dekóduj URL – protože při předání přes query string bude zakódovaná
     const decodedUrl = decodeURIComponent(rawUrl);
 
-    // Kontrola domény
     if (!decodedUrl.startsWith('https://www.cedok.cz/')) {
       return res.status(400).json({ error: 'URL musí začínat na https://www.cedok.cz/' });
     }
 
-    // Stáhni obsah stránky
     const response = await axios.get(decodedUrl, {
       headers: {
         'User-Agent': 'Mozilla/5.0'
@@ -24,10 +22,27 @@ module.exports = async function (req, res) {
     });
 
     const html = response.data;
+    const $ = cheerio.load(html);
+
+    const title = $('title').text();
+    const metaDescription = $('meta[name="description"]').attr('content') || null;
+
+    const h1 = $('h1').map((i, el) => $(el).text()).get();
+    const h2 = $('h2').map((i, el) => $(el).text()).get();
+    const h3 = $('h3').map((i, el) => $(el).text()).get();
+
+    const paragraphs = $('p').map((i, el) => $(el).text()).get().slice(0, 5); // jen prvních 5 odstavců
 
     return res.status(200).json({
       originalUrl: decodedUrl,
-      html
+      title,
+      metaDescription,
+      headings: {
+        h1,
+        h2,
+        h3
+      },
+      paragraphs
     });
 
   } catch (err) {
